6592d606c30d0c6700ff317ad0148492f2d88c44
SocratesMorreu <lfjanot87@gmail.com>
2023-05-02 20:54:56 -0300

    comentarios feitos

--- main/jogo.py
+++ main/jogo.py
@@ -2,9 +2,35 @@ import pygame
 from telas import *
 
 class Jogo:
-    
+    """
+    Classe responsável por gerenciar o jogo e suas telas.
+
+    Atributos:
+    -----------
+    sprites : pygame.sprite.Group
+        Grupo de sprites do jogo.
+    window : pygame.Surface
+        Tela principal do jogo.
+    tela_atual : Tela
+        Tela atual do jogo.
+    last_updated : int
+        Timestamp do último update do jogo.
+
+    Métodos:
+    --------
+    __init__()
+        Inicializa a classe Jogo.
+    recebe_eventos()
+        Recebe e trata eventos do jogo.
+    game_loop()
+        Loop principal do jogo.
+    finaliza()
+        Finaliza o jogo e a biblioteca pygame.
+    """
     def __init__(self):
-        
+        """
+        Inicializa a classe jogo
+        """
         pygame.init()
         self.sprites = pygame.sprite.Group()
 
@@ -14,7 +40,9 @@ class Jogo:
         self.last_updated = pygame.time.get_ticks()
 
     def recebe_eventos(self):
-        
+        """
+        Recebe todos os eventos de todas as telas do jogo 
+        """
         self.tela_atual = self.tela_atual.recebe_eventos()
         # recebe_eventos tela atual
         if self.tela_atual is None:
@@ -22,12 +50,17 @@ class Jogo:
         return True
 
     def game_loop(self):
-        
+        """
+        Loop principal do jogo
+        """
         while self.recebe_eventos():
             self.tela_atual.desenha(self.window)
             pygame.display.update()
 
     def finaliza(self):
+        """
+        Finaliza o jogo 
+        """
         pygame.quit()
 
 if __name__ == '__main__':
